149cc6a7ab1e3ccb11407a372b1ad1cf5ba7ffee
Khaleel <khaleelshaik711@gmail.com>
2025-10-23 15:26:44 +0530

    terraform first example

---
+++ expense-v1/servers.tf
@@ -0,0 +1,24 @@
+resource "aws_instance" "frontend" {
+  ami           = ami-09c813fb71547fc4f
+  instance_type = "t3.small"
+
+  tags = {
+    Name = "frontend"
+  }
+}
+resource "aws_instance" "backend" {
+  ami           = ami-09c813fb71547fc4f
+  instance_type = "t3.small"
+
+  tags = {
+    Name = "backend"
+  }
+}
+resource "aws_instance" "mysql" {
+  ami           = ami-09c813fb71547fc4f
+  instance_type = "t3.small"
+
+  tags = {
+    Name = "mysql"
+  }
+}
