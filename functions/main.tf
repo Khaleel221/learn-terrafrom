e16b8a71b1361dfbd13d5a35561cb3148e2f8a2e
Khaleel <khaleelshaik711@gmail.com>
2025-10-24 12:53:15 +0530

    terraform functions

--- functions/main.tf
+++ functions/main.tf
@@ -1,9 +1,9 @@
-variable "fruits"{}
+variable "fruits" {}
 
 output "apple" {
-  value = "apple qty - ${lookup(var.fruits["apple"]), "qty", 0]}, price is - ${lookup(var.fruits["apple"]), "price", 0.5]}"
+  value = "apple qty - ${lookup(var.fruits["apple"], "qty", 0)}, price is - ${lookup(var.fruits["apple"], "price", 0.5)}"
 }
 
 output "banana" {
-  value = "banana qty - ${lookup(var.fruits["banana"]), "qty", 0]}, price is - ${lookup(var.fruits["banana"]), "price", 0.5]}"
+  value = "banana qty - ${lookup(var.fruits["banana"], "qty", 0)}, price is - ${lookup(var.fruits["banana"], "price", 0.5)}"
 }
