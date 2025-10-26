4550c5abf483d5bd520613904926fb701d64522d
Khaleel <khaleelshaik711@gmail.com>
2025-10-26 13:38:36 +0530

    terraform modules

--- expense-v5/main.tf
+++ expense-v5/main.tf
@@ -5,6 +5,6 @@ module "app" {
   ami                    = data.aws_ami.ami.image_id
   instance_type          = each.value["instance_type"]
   name                   = each.key
-  vpc_security_group_ids = data.aws_security_group.allow-all.vpc_id
+  vpc_security_group_ids = [data.aws_security_group.allow-all.vpc_id]
   zone_id                = data.aws_route53_zone.main.zone_id
 }
