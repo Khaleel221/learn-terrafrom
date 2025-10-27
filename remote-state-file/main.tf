ccb9fd7b0785705281cfc1edb490477ad2c55bf0
Khaleel <khaleelshaik711@gmail.com>
2025-10-27 12:55:22 +0530

    terraform state file

--- remote-state-file/main.tf
+++ remote-state-file/main.tf
@@ -2,7 +2,7 @@ resource "null_resource" "test" {}
 
 terraform {
   backend "s3" {
-    bucket = "d79-terraform"
+    bucket = "devops79-terraform"
     key    = "test/terraform.tfstate"
     region = "us-east-1"
   }
